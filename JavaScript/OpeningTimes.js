ab37bd4734eb41a8f9fb6564c54c0125969a52d8
hotbloxer <33450949+hotbloxer@users.noreply.github.com>
2025-03-19 15:48:51 +0100

    pizza upadte

---
+++ JavaScript/OpeningTimes.js
@@ -0,0 +1,29 @@
+async function getOpeningTimes (){
+    const url = "JSON/OpeningTimes.json";
+    let textToChange = document.getElementById("openingTimes");
+
+    try
+    {
+        const response = await fetch(url);
+        if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+        }
+        console.log("test 123");
+        const json = await response.json();
+        console.log(json);
+        textToChange.innerHTML = json.times[0].name;
+
+        textToChange.innerHTML = "";
+        for (let i = 0; i < json.times.length ; i++) {
+            console.log("test " + i);
+            textToChange.innerHTML += "<div class= " + "day" + ">" + json.times[i].name;
+            textToChange.innerHTML += "<div class= " + "time" + ">" + json.times[i].open;
+            textToChange.innerHTML += "<div class= " + "time" + ">" + json.times[i].close;
+        }
+    } catch (error) 
+        {
+            console.error(error.message);
+        }
+}
+
+getOpeningTimes();
--- JavaScript/Pizzas.js
+++ JavaScript/Pizzas.js
@@ -2,7 +2,7 @@
 
 async function getPizzaMenu (){
     const url = "JSON/Pizzas.json";
-    let textToChange = document.getElementById("menueCard");
+    let textToChange = document.getElementById("pizzaMenu");
 
     try
     {
@@ -12,23 +12,52 @@ async function getPizzaMenu (){
         }
 
         const json = await response.json();
-        console.log(json);
+       
         textToChange.innerHTML = json.pizzas[0].name;
 
-        textToChange.innerHTML = "<ul>";
-        for (let i = 0; i < json.pizzas.length ; i++) {
-            console.log("test " + i);
-            textToChange.innerHTML += "<li>";
-            textToChange.innerHTML += json.pizzas[i].name;;
-            textToChange.innerHTML += "</li>";
+        textToChange.innerHTML = "";
+        let counter = 0;
+    
+        console.log(json.pizzas.length);
 
+        for (let i = 0; i < json.pizzas.length ; i++) {
+            if (counter == 0 ) {
+                counter ++;
+                textToChange.innerHTML += "<div> </div>";
+           
+            }
+
+            if (counter == 4) {
+                textToChange.innerHTML += "<div> </div>";
+                textToChange.innerHTML += "<div> </div>";
+                counter = 0;
+                
+            }
+            
+            textToChange.innerHTML += 
+            "<div class = " 
+            + "fixedHeightMenu>" 
+            + "<figure >" 
+            + "<img class= fitImgToContainer src =" + json.pizzas[i].img 
+            + ">" 
+            + "<caption>" 
+            + json.pizzas[i].name 
+            + "</caption>" 
+            + "</figure >" 
+            + "</div>";
+            counter ++;
         }
-        textToChange.innerHTML += "</ul>";
+    
+
     } catch (error) 
         {
             console.error(error.message);
         }
+
+       
 }
+
+
 
 let tetst = getPizzaMenu();
 
